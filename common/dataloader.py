6b7215e70b49cf79b3f4a6e4226f8c03faa259c4
wuyenlin <finne0831@gmail.com>
2021-07-14 19:54:16 +0200

    re-ordered h36m 2d and 3d

--- common/dataloader.py
+++ common/dataloader.py
@@ -39,9 +39,8 @@ class Data:
                 if action is None else random.sample(list(frames), int(len(frames)))
             num_frame += len(reduced)
             for f in reduced:
-                gt_2d = self.zero_center(frames[f]["positions_2d"])
-                gt_3d = self.zero_center(self.remove_joints( \
-                        frames[f]["positions_3d"]))
+                gt_2d = self.zero_center(self.remap_h36m(frames[f]["positions_2d"]))
+                gt_3d = self.zero_center(self.remove_joints(frames[f]["positions_3d"]))
 
                 assert gt_2d.shape == (17,2) and gt_3d.shape == (17,3)
                 self.gt_pts2d.append(gt_2d)
@@ -74,9 +73,26 @@ class Data:
         keep = [0,1,2,3,6,7,8,12,13,14,15,17,18,19,25,26,27]
         for row in range(17):
             new_skel[row, :] = kpts[keep[row], :]
+        new_skel = self.remap_h36m(new_skel)
         return new_skel
 
 
     def zero_center(self, cam) -> np.array:
         """translate root joint to origin (0,0,0)"""
-        return cam - cam[0,:]
+        return cam - cam[2,:]
+
+    def remap_h36m(self, h36m_joints):
+        """
+        convert Human3.6M joint order
+        to normal MPI-INF-3DHP one
+        """
+        mpi_joints = np.zeros_like(h36m_joints)
+        replace_list = (
+            (0,8), (1,7), (2,0), (3,9), (4,10),
+            (5,11), (6,12), (7,13), (8,14), (9,15), (10,16),
+            (11,4), (12,5), (13,6), (14,1), (15,2), (16,3)
+        )
+        for joint in replace_list:
+            mpi_joints[joint[0]] = h36m_joints[joint[1]]
+
+        return mpi_joints
